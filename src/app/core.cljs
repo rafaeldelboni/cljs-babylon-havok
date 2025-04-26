b2b5e52d628b0ad3863513f2f3170ab5e446f40d
Rafael Delboni <rafadelboni@gmail.com>
2025-04-25 22:45:15 -0300

    feat: using gltf scene to render physics test

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -2,17 +2,17 @@
   (:require
    ["@babylonjs/core/Cameras/freeCamera.js" :refer [FreeCamera]]
    ["@babylonjs/core/Engines/engine.js" :refer [Engine]]
-   ["@babylonjs/core/Lights/hemisphericLight.js" :refer [HemisphericLight]]
+   ["@babylonjs/core/Loading/loadingScreen"]
+   ["@babylonjs/core/Loading/sceneLoader.js" :refer [AppendSceneAsync]]
+   ["@babylonjs/core/Materials/standardMaterial"]
    ["@babylonjs/core/Maths/math.vector.js" :refer [Vector3]]
-   ["@babylonjs/core/Meshes/Builders/groundBuilder.js" :refer [CreateGround]]
-   ["@babylonjs/core/Meshes/Builders/boxBuilder.js" :refer [CreateBox]]
-   ["@babylonjs/core/scene.js" :refer [Scene]]
-   ["@babylonjs/core/Physics/v2/Plugins/havokPlugin.js" :refer [HavokPlugin]]
+   ["@babylonjs/core/Physics/physicsEngineComponent.js"]
    ["@babylonjs/core/Physics/v2/IPhysicsEnginePlugin.js" :refer [PhysicsShapeType]]
    ["@babylonjs/core/Physics/v2/physicsAggregate.js" :refer [PhysicsAggregate]]
-   ["@babylonjs/core/Physics/physicsEngineComponent.js"]
-   ["@babylonjs/materials/simple/simpleMaterial.js" :refer [SimpleMaterial]]
-   ["@babylonjs/havok$default" :as HavokPhysics]))
+   ["@babylonjs/core/Physics/v2/Plugins/havokPlugin.js" :refer [HavokPlugin]]
+   ["@babylonjs/core/scene.js" :refer [Scene]]
+   ["@babylonjs/havok$default" :as HavokPhysics]
+   ["@babylonjs/loaders/dynamic" :refer [registerBuiltInLoaders]]))
 
 (defn- prepare-container! [id]
   (let [container (js/document.getElementById id)]
@@ -26,28 +26,30 @@
                                              :preserveDrawingBuffer true
                                              :disableWebGL2Support false})
         scene (new Scene engine)
-        material (new SimpleMaterial "simple" scene);
-        camera (new FreeCamera "camera1" (new Vector3 0 5 -10) scene)
-        light (new HemisphericLight "light" (new Vector3 0 1 0) scene)
-        box1 (CreateBox "box1" #js {:width 1 :height 1} scene)
-        box2 (CreateBox "box1" #js {:width 1 :height 1} scene)
-        ground (CreateGround "ground" #js {:width 10 :height 10})]
+        camera (new FreeCamera "camera1" (new Vector3 0 5 -10) scene)]
 
-    (.setTarget camera (. Vector3 Zero))
-    (.attachControl camera canvas true)
-    (set! (.-material box1) material)
-    (set! (.-material box2) material)
-    (set! (.-material ground) material)
-    (set! (.-intensity light) 0.7)
-    (set! (-> box1 .-position .-y) 2.5)
-    (set! (-> box2 .-position .-y) 5)
-    (set! (-> box2 .-position .-x) 0.5)
-    (.enablePhysics scene (new Vector3 0 -9.8 0) hk)
-    (new PhysicsAggregate box1 (.-BOX PhysicsShapeType) #js {:mass 1} scene)
-    (new PhysicsAggregate box2 (.-BOX PhysicsShapeType) #js {:mass 1} scene)
-    (new PhysicsAggregate ground (.-BOX PhysicsShapeType) #js {:mass 0} scene)
-    (js/console.log "aaaaaaa" scene)
-    (.runRenderLoop engine (fn [] (when scene (.render scene))))))
+    (-> (AppendSceneAsync "assets/test-scene.glb" scene)
+        (.then (fn []
+                 (.setTarget camera (. Vector3 Zero))
+                 (.attachControl camera canvas true)
+                 (.enablePhysics scene (new Vector3 0 -9.8 0) hk)
+
+                 (doseq [^js mesh (.-meshes scene)]
+                   (when-let [metadata (.-metadata mesh)]
+                     (let [extras (-> metadata .-gltf .-extras)
+                           type (.-collider_type extras)
+                           shape (.-collider_shape extras)]
+                       (case [type shape]
+                         ["static" "box"]
+                         (new PhysicsAggregate mesh (.-BOX PhysicsShapeType) #js {:mass 0} scene)
+                         ["dynamic" "box"]
+                         (new PhysicsAggregate mesh (.-BOX PhysicsShapeType) #js {:mass 1} scene)
+                         ["dynamic" "sphere"]
+                         (new PhysicsAggregate mesh (.-SPHERE PhysicsShapeType) #js {:mass 1 :restitution 0.5} scene)))))
+
+                 (.runRenderLoop engine (fn [] (when scene (.render scene)))))))))
+
+(registerBuiltInLoaders)
 
 (-> (HavokPhysics #js {:locateFile (fn [_] "havok.wasm")})
     (.then app))
