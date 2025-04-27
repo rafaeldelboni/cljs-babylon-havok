5954a0b812c4d005cc714d87b01fcf32377d29c7
Rafael Delboni <rafadelboni@gmail.com>
2025-04-26 21:58:55 -0300

    feat: add global light

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,16 +1,17 @@
 (ns app.core
   (:require
-   ["@babylonjs/core/Cameras/freeCamera.js" :refer [FreeCamera]]
-   ["@babylonjs/core/Engines/engine.js" :refer [Engine]]
+   ["@babylonjs/core/Cameras/freeCamera" :refer [FreeCamera]]
+   ["@babylonjs/core/Engines/engine" :refer [Engine]]
+   ["@babylonjs/core/Lights" :refer [HemisphericLight]]
    ["@babylonjs/core/Loading/loadingScreen"]
-   ["@babylonjs/core/Loading/sceneLoader.js" :refer [AppendSceneAsync]]
+   ["@babylonjs/core/Loading/sceneLoader" :refer [AppendSceneAsync]]
    ["@babylonjs/core/Materials/standardMaterial"]
-   ["@babylonjs/core/Maths/math.vector.js" :refer [Vector3]]
-   ["@babylonjs/core/Physics/physicsEngineComponent.js"]
-   ["@babylonjs/core/Physics/v2/IPhysicsEnginePlugin.js" :refer [PhysicsShapeType]]
-   ["@babylonjs/core/Physics/v2/physicsAggregate.js" :refer [PhysicsAggregate]]
-   ["@babylonjs/core/Physics/v2/Plugins/havokPlugin.js" :refer [HavokPlugin]]
-   ["@babylonjs/core/scene.js" :refer [Scene]]
+   ["@babylonjs/core/Maths/math.vector" :refer [Vector3]]
+   ["@babylonjs/core/Physics/physicsEngineComponent"]
+   ["@babylonjs/core/Physics/v2/IPhysicsEnginePlugin" :refer [PhysicsShapeType]]
+   ["@babylonjs/core/Physics/v2/physicsAggregate" :refer [PhysicsAggregate]]
+   ["@babylonjs/core/Physics/v2/Plugins/havokPlugin" :refer [HavokPlugin]]
+   ["@babylonjs/core/scene" :refer [Scene]]
    ["@babylonjs/havok$default" :as HavokPhysics]
    ["@babylonjs/loaders/dynamic" :refer [registerBuiltInLoaders]]))
 
@@ -18,7 +19,7 @@
   (let [container (js/document.getElementById id)]
     (set! (.-innerHTML container) "")
     container))
-
+;
 (defn app [havok-instance]
   (let [hk (new HavokPlugin true havok-instance);
         canvas (prepare-container! "render")
@@ -26,10 +27,13 @@
                                              :preserveDrawingBuffer true
                                              :disableWebGL2Support false})
         scene (new Scene engine)
+        global-light (new HemisphericLight "HemiLight" (new Vector3 0 1 0) scene)
         camera (new FreeCamera "camera1" (new Vector3 0 5 -10) scene)]
 
     (-> (AppendSceneAsync "assets/test-scene.glb" scene)
         (.then (fn []
+                 (set! (.-intensity global-light) 0.5)
+
                  (.setTarget camera (. Vector3 Zero))
                  (.attachControl camera canvas true)
                  (.enablePhysics scene (new Vector3 0 -9.8 0) hk)
